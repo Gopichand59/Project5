6f5f0005ef85983314895b2497e6183ff671917a
Gopichand <122634178+Gopichand59@users.noreply.github.com>
2023-11-25 23:42:56 -0600

    Add files via upload

---
+++ src/main/java/com/example/DMS/repository/DogRepository.java
@@ -0,0 +1,18 @@
+/**
+ * Gopichand Chandhana
+ */
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Dog;
+
+
+public interface DogRepository extends CrudRepository<Dog, Integer> {
+	List<Dog> findByName(String name);
+	
+	
+
+}
---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,15 @@
+/**
+ * Gopichand
+ */
+package com.eample.DMS.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+	
+
+}
